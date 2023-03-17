4865cce4a2731899012af1398d332ce7d6ac742e
Sergey Popov <popow_sv@ukr.net>
2023-03-17 15:03:26 +0200

    My homework11 main corrected

--- src/main/java/org/hillel/homework11/Main.java
+++ src/main/java/org/hillel/homework11/Main.java
@@ -1,7 +1,11 @@
 package org.hillel.homework11;
 
+import org.Hillel.mathematic.Mathematic;
+
 public class Main {
   public static void main(String[] args) {
-
+    Mathematic mathematic = new Mathematic();
+    System.out.println(mathematic.exponentiation(4, 2));
   }
+
 }
