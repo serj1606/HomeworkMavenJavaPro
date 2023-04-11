c9a8f3c1efbe405000494f1dcfeb793ad50c3b82
Sergey Popov <popow_sv@ukr.net>
2023-04-11 18:48:57 +0300

    My homework AlgorithmsTest fixed

--- homework_algorithms/src/test/java/org/hillel/algorithms/AlgorithmsTest.java
+++ homework_algorithms/src/test/java/org/hillel/algorithms/AlgorithmsTest.java
@@ -5,42 +5,44 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AlgorithmsTest {
-
   @Test
-  public void cocktailSortTest() {
+  public void cocktailSortedArrayTest() {
     Algorithms algorithms = new Algorithms();
-
-    // test sorted array
     int[] sortedArray = new int[] {1, 4, 6, 7, 8};
     algorithms.cocktailSort(sortedArray);
     Assertions.assertArrayEquals(new int[] {1, 4, 6, 7, 8}, sortedArray);
-
-    // test unsorted array
+  }
+  @Test
+  public void cocktailUnsortedArrayTest() {
+    Algorithms algorithms = new Algorithms();
     int[] unsortedArray = new int[] {1, 8, 6, 7, 4};
     algorithms.cocktailSort(unsortedArray);
     Assertions.assertArrayEquals(new int[] {1, 4, 6, 7, 8}, unsortedArray);
-
-    // test empty array
+  }
+  @Test
+  public void cocktailEmptyArrayTest() {
+    Algorithms algorithms = new Algorithms();
     int[] emptyArray = new int[0];
     algorithms.cocktailSort(emptyArray);
     Assertions.assertArrayEquals(new int[] {}, emptyArray);
   }
-
   @Test
-  public void quickSortTest() {
+  public void quickSortedArrayTest() {
     Algorithms algorithms = new Algorithms();
-
-    // test sorted array
     int[] sortedArray = new int[] {1, 2, 3, 4, 5};
     algorithms.quickSort(sortedArray, 0, sortedArray.length - 1);
     Assertions.assertArrayEquals(new int[] {1, 2, 3, 4, 5}, sortedArray);
-
-    // test unsorted array
+  }
+  @Test
+  public void quickSortedUnsortedArrayTest() {
+    Algorithms algorithms = new Algorithms();
     int[] unsortedArray = new int[] {5, 4, 3, 2, 1};
     algorithms.quickSort(unsortedArray, 0, unsortedArray.length - 1);
     Assertions.assertArrayEquals(new int[] {1, 2, 3, 4, 5}, unsortedArray);
-
-    // test empty array
+  }
+  @Test
+  public void quickSortedEmptyArrayTest() {
+    Algorithms algorithms = new Algorithms();
     int[] emptyArray = new int[0];
     algorithms.quickSort(emptyArray, 0, emptyArray.length - 1);
     Assertions.assertArrayEquals(new int[] {}, emptyArray);
